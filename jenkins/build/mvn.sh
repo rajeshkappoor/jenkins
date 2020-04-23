8c1a36c6a5ead824518f4a6d38a2dfecec6d4ebc
Ubuntu <rajcomeng@gmail.com>
2020-04-23 23:52:50 +0000

    Final changes completed

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,5 @@ echo "************************"
 echo "****Building JAR *******"
 echo "************************"
 
-echo $WORKSPACE
-
+WORKSPACE=/home/ubuntu/jenkins/jenkins_home/workspace/pipeline-docker-maven
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3-alpine "$@"
