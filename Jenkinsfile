bc10191347edb15cba19cf878be1820f5851f7c2
Ubuntu <rajcomeng@gmail.com>
2020-04-23 23:36:05 +0000

    With all the required changes

--- Jenkinsfile
+++ Jenkinsfile
@@ -17,12 +17,24 @@ pipeline {
 					
                 '''
             }
+
+            post {
+                success {
+                   archiveArtifacts artifacts: 'java-app/target/*.jar', fingerprint: true
+                }
+            }			
         
         }
 
         stage('Test') {
             steps {
                 sh './jenkins/test/mvn.sh mvn test'
+            }
+
+            post {
+                always {
+                    junit 'java-app/target/surefire-reports/*.xml'
+                }
             }
         
         }
--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /home/ubuntu/jenkins/prod /tmp/.auth jenkins@18.212.158.161:/tmp/.auth
-scp -i /home/ubuntu/jenkins/prod ./jenkins/deploy/publish.sh jenkins@18.212.158.161:/tmp/publish.sh
-ssh -i /home/ubuntu/jenkins/prod jenkins@18.212.158.161 "/tmp/publish.sh"
+scp -i /opt/prod /tmp/.auth jenkins@18.212.158.161:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish.sh jenkins@18.212.158.161:/tmp/publish.sh
+ssh -i /opt/prod jenkins@18.212.158.161 "/tmp/publish.sh"
