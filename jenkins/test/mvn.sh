292c2148446d5452d8880bdf94272058e74d29d3
Ubuntu <rajcomeng@gmail.com>
2020-04-23 23:07:50 +0000

    Fixed workspace directory

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,4 +4,6 @@ echo "************************"
 echo "**Testing the code******"
 echo "************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3-alpine "$@"
+echo $WORKSPACE
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3-alpine "$@"
