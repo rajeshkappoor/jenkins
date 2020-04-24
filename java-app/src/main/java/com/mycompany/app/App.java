b9f4368dc26a1b913e92b3491125a11bc61f06fc
Ubuntu <rajcomeng@gmail.com>
2020-04-24 02:03:26 +0000

    Final test

--- java-app/src/main/java/com/mycompany/app/App.java
+++ java-app/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World 2020!";
+    private final String message = "Hello World 2021!";
 
     public App() {}
 
--- java-app/src/test/java/com/mycompany/app/AppTest.java
+++ java-app/src/test/java/com/mycompany/app/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World 2020!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello World 2021!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
