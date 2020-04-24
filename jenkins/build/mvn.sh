cf340c6e737a7bba6057754600c9e73c85662fa5
Ubuntu <rajcomeng@gmail.com>
2020-04-24 01:57:01 +0000

    test456

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "************************"
-echo "****Building JAR5 *******"
+echo "****Building JAR6 *******"
 echo "************************"
 
 WORKSPACE=/home/ubuntu/jenkins/jenkins_home/workspace/pipeline-docker-maven
