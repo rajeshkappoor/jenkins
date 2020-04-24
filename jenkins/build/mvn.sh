bfa501c504d4bc1b0f68461f5017c8594326cf32
Ubuntu <rajcomeng@gmail.com>
2020-04-24 01:55:15 +0000

    test

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo "************************"
-echo "****Building JAR6 *******"
+echo "****Building JAR5 *******"
 echo "************************"
 
 WORKSPACE=/home/ubuntu/jenkins/jenkins_home/workspace/pipeline-docker-maven
-docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3-alpine "$@"
+docker run --rm -u 1000 -e MAVEN_CONFIG=/app -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3-alpine "$@"
